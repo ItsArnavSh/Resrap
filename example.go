98e0e48eba3098233e56f4fccb9f7dd1727fbc35
ItsArnavSh <its.arnav.sh@gmail.com>
2025-09-26 19:46:33 +0530

    Teeny Tiny Error

--- example.go
+++ example.go
@@ -7,7 +7,7 @@ import (
 
 func examplecode() {
 	graphs := resrap.NewResrap()
-	graphs.ParseGrammarFile("C", "grammar/C.g4")
+	graphs.ParseGrammarFile("C", "example/C.g4")
 	random_content := graphs.GenerateRandom("C", "program")
 	fmt.Println(random_content)
 	seeded_content := graphs.GenerateWithSeeded("C", "program", 20)
